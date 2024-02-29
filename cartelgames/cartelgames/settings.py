0c1ec7fc6f4396d67ca4660456115e013df0fe2e
Lulu <lu.ca.deville@outlook.fr>
2024-02-29 20:39:42 +0100

    Prep .sh for database PostGres | login page created | fix index

--- cartelgames/cartelgames/settings.py
+++ cartelgames/cartelgames/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cartelgames.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transcendence',
+        'USER': 'cartel',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- cartelgames/cartelgames/urls.py
+++ cartelgames/cartelgames/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('transcendence/', include('transcendence.urls')),
+    path('', include('transcendence.urls')),
 ]
