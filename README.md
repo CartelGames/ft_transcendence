7eb1ffb98b99379d497ca0fa640fd390a9cae95c
Saezito <hip66@hotmail.fr>
2024-03-04 20:21:34 +0100

    test de checkout sur ma branch

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ft_transcendence
+
+Pour l'instant il suffit de lancer via Django en étant dans le dossier cartelgames :
+# python3 manage.py runserver
+
+Et ensuite d'aller sur cet url :
+# http://localhost:8000/transcendence/index
+
+In collaboration with [@LucasDeville](https://github.com/LucasDeville), [@saezito](https://github.com/SAEZITO), [@Thawnder](https://github.com/Thawnder), [@jordancasadessus](https://github.com/jordancasadessus) and [@Paloouf](https://github.com/Paloouf)
---
+++ app/static/js/script.js
@@ -0,0 +1,63 @@
+var upHist = false;
+
+function showDiv(divId) {
+    var divs = document.querySelectorAll('.block');
+    divs.forEach(function (div) {
+        div.style.display = 'none';
+    });
+
+    var selectedDiv = document.getElementById(divId);
+    if (selectedDiv) {
+        selectedDiv.style.display = 'block';
+
+        if (!upHist) {
+			var stateObj = { divId: divId };
+			var url = window.location.href.split('#')[0] + '#' + divId;
+			history.pushState(stateObj, null, url);
+		}
+		else
+			upHist = false;
+    }
+}
+
+function sendForm(id, event) {
+    var form = document.getElementById(id);
+    var formData = new FormData(form);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', form.action, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            var errorForm = form.querySelector('.error-form');
+            if (response.success) {
+                if (errorForm)
+                    errorForm.innerHTML = response.errors;
+                if (response.goto) {
+                    setTimeout(function () {
+                        window.location.href = response.goto;
+                        if (errorForm)
+                            errorForm.innerHTML = "";
+                    }, 3000);
+                }
+            } else {
+                if (errorForm)
+                    errorForm.innerHTML = response.errors;
+            }
+        }
+    };
+    xhr.send(formData);
+    event.preventDefault();
+}
+
+window.addEventListener('hashchange', function () {
+	var divId = location.hash.slice(1) || 'index';
+	upHist = true;
+	console.log("Call hashchange : " + divId);
+    showDiv(divId);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var defaultDivId = location.hash.slice(1) || 'index';
+	console.log("First call : " + defaultDivId);
+    showDiv(defaultDivId);
+});
