94465687c1ea10fd0ef4a59fc49303ff94f87ebc
paloouf <paloouf@gmail.com>
2024-03-11 17:23:44 +0100

    le pong avance bien

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,13 +1,26 @@
+import { FontLoader, TextGeometry } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+
 const scene = new THREE.Scene();
 const canvas = document.getElementById("game");
 const camera = new THREE.PerspectiveCamera(75, canvas.width/canvas.height, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#game'),
 });
-
-renderer.setPixelRatio(2);
-renderer.setSize(canvas.width, canvas.height, false);
+const camHelper = new THREE.CameraHelper( camera );
+scene.add(camHelper);
+camHelper.update();
 camera.position.z =30;
+
+
+// const lineSegment = camHelper.children[0]; // get the first LineSegment object in the helper
+// console.log("linesegmentstart", lineSegment.start);
+// const size = lineSegment.start.distanceTo(lineSegment.end);
+// console.log('Size of LineSegment:', size);
+
+const mConvert = 0.0002645833;
+renderer.setPixelRatio(canvas.width/canvas.height);
+renderer.setSize(canvas.width, canvas.height, false);
+
 
 //the big pink floating thingy
 let boardHeight = 10;
@@ -21,13 +34,21 @@ const playerOne = new THREE.Mesh( geometry, material );
 const playerTwo = new THREE.Mesh( geometry, material );
 const ball = new THREE.Mesh( ballHelp, material);
 scene.add( playerOne , playerTwo, ball, back);
+const canvasBounds = {
+  left: -((canvas.width/2) * mConvert) * (camera.position.z * 9.3),
+  right: ((canvas.width/2) * mConvert)* (camera.position.z * 9.3),
+  bottom: -((canvas.height/2) * mConvert)* camera.position.z * 9.3,
+  top: ((canvas.height/2) * mConvert)* camera.position.z * 9.3
+};
 
 back.position.set(0,0,-10);
-ball.position.set(0,0,4);
-playerOne.position.set(-35, 0, 4);
+ball.position.set(0,0,0);
+playerOne.position.set(canvasBounds.left + 2, 0, 0);
+playerTwo.position.set(canvasBounds.right - 2, 0, 0);
 
-playerTwo.position.set(35, 0, 4);
-
+let score = new Array(2);
+score[0]=0;
+score[1]=0;
 
 //on met de la lumiere
 const pointLight = new THREE.PointLight(0xff00ff);
@@ -42,16 +63,12 @@ scene.add(pointLight);
 document.addEventListener('keydown', onKeyDown);
 document.addEventListener('keyup', onKeyUp)
 
-const canvasBounds = {
-  left: -100,
-  right: 100,
-  bottom: -20,
-  top: 20
-};
 
+
+console.log(canvasBounds.left, canvasBounds.right, canvasBounds.top, canvasBounds.bottom);
 // canvas.width
 // canvas.height
-let ballSpeed = 0.1;
+let ballSpeed = 0.2;
 let ballDirection = { x: 1, y: 1 };
 
 const keyState = {};
@@ -65,14 +82,27 @@ const keyState = {};
     keyState[event.keyCode] = false; 
   }
 
+  scoring();
   function update() {
 
     ball.position.x += ballSpeed * ballDirection.x;
     ball.position.y += ballSpeed * ballDirection.y;
     const ballX = (ball.position.x * camera.zoom) * Math.tan(camera.fov / 2) * camera.aspect;
     const ballY = (ball.position.y * camera.zoom) * Math.tan(camera.fov / 2);
-    if (ball.position.x < canvasBounds.left || ball.position.x > canvasBounds.right) {
-      ballDirection.x *= -1; // reverse the X direction of the ball
+    if (ball.position.x < canvasBounds.left){
+      ballSpeed = 0.2;
+      ball.position.set(0,0,0);
+      ballDirection = {x: -1, y: 1}
+      score[0]++;
+      scoring();
+      console.log(score[0]);
+    }
+    if (ball.position.x > canvasBounds.right) {
+      ballSpeed = 0.2;
+      ball.position.set(0,0,0);
+      score[1]++; // reverse the X direction of the ball
+      scoring();
+      console.log(score[1]);
     }
   
     // Check for bottom and top boundary collisions
@@ -86,7 +116,7 @@ const keyState = {};
         ball.position.y < playerOne.position.y + boardHeight/2 &&
         ball.position.y > playerOne.position.y - boardHeight/2 && ballDirection.x < 0) {
       ballDirection.x *= -1; // reverse the X direction of the ball
-      //ballSpeed += 0.05;
+      ballSpeed *= 1.1;
     }
   
     // Check for playerTwo collision
@@ -95,7 +125,7 @@ const keyState = {};
         ball.position.y < playerTwo.position.y + boardHeight/2 &&
         ball.position.y > playerTwo.position.y - boardHeight/2 && ballDirection.x > 0) {
       ballDirection.x *= -1; // reverse the X direction of the ball
-      //ballSpeed += 0.05;
+      ballSpeed *= 1.1;
     }
 
 
@@ -135,6 +165,7 @@ const keyState = {};
       movePong(playerTwo, playerTwo.position.y + 4);
     if (keyState[40])
       movePong(playerTwo, playerTwo.position.y - 4);
+      
   }
 
   function movePong(mesh, targetY) {
@@ -143,19 +174,41 @@ const keyState = {};
       ease: "power2.out", // easing function to use
       y: targetY, // target y-axis position
     });
-    
   }
-  
 
+  function scoring(){
+    const loader = new FontLoader();
+        let font_src = 'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/fonts/helvetiker_regular.typeface.json';
+        loader.load( font_src, function ( font ) {
+        
+          const geometry = new TextGeometry( score[0].toString(), {
+            font: font,
+            size: 1,
+            height: 0,
+          } );
+          const geometry2 = new TextGeometry( score[1].toString(), {
+            font: font,
+            size: 1,
+            height: 0,
+          } );
+          const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        const textMesh = new THREE.Mesh(geometry, textMaterial);
+        textMesh.position.set(-10, 10, 0);
+        const textMesh2 = new THREE.Mesh(geometry2, textMaterial);
+        textMesh2.position.set(10, 10, 0);
+        scene.remove(textMesh, textMesh2);
+        scene.add(textMesh, textMesh2);
+        } );
+    }
 //pour mettre une image en background (on rajoute le path dans le html)
 // const imageUrl = document.getElementById('image-url').value;
 // const backgroundTexture = new THREE.TextureLoader().load(imageUrl);
 // scene.background = backgroundTexture;
 
-
 //fonction pour refresh le render et faire bouger le big pink thingy
 function animate() {
     requestAnimationFrame(animate);
+    //console.log("sphere", ball.intersectsObject());
     update();
     renderer.render(scene, camera);
 }
