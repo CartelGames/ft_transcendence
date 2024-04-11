81301b69c035dfb443bd22abbaadfbecc38bf039
Jordan <jordan.casadessus@gmail.com>
2024-04-11 18:55:26 +0200

    mise à niveau du main

--- app/static/js/queue.js
+++ app/static/js/queue.js
@@ -9,6 +9,8 @@ websocket.onmessage = function(event) {
     var data = JSON.parse(event.data);
     console.log('Event Queue Socket : ' + data.type);
     if (data.type === 'game_start') {
+        var HideDiv = document.getElementById('LeaveQueue');
+        HideDiv.style.display = 'none';
         import('/static/js/game.js?ver=${Math.random()}')
         .then(module => {
             const { reloadGame } = module;
@@ -50,6 +52,181 @@ function LeaveQueue() {
     showDiv.style.display = 'block';
 }
 
+function TournamentInfo(id) {
+    displayDiv('ChooseTour', 'InfoTour');
+    $.ajax({
+        type: 'GET',
+        url: '/getTournamentInfo/?id=' + id,
+        headers: { 'X-CSRFToken': token },
+        success: function (data) {
+            if (data.success) {
+                var TournamentContainer = $('#InfoTourUpdate');
+                TournamentContainer.empty();
+                var TourInfo = data.tourList;
+                var TourPlayers = data.player;
+                var Title =$('<h1 style="font-weight: bold; font-size: 26px">Tournament name : ' + TourInfo[0].name + '</h1>');
+                var Title2 =$('<h1 style="font-weight: bold; font-size: 20px"><i>Created by ' + TourInfo[0].creator + '</i></h1><hr /><br />');
+                TournamentContainer.append(Title);
+                TournamentContainer.append(Title2);
+                var Players =$('<h1 style="font-weight: bold; font-size: 24px">Players registered (' + TourInfo[0].players + ') :</h1>');
+                var PlayersList =$('<p></p>');
+                TourPlayers.forEach(function (player) {
+                    var pseudo =$('<span> ' + player.pseudo + ',</span>');
+                    PlayersList.append(pseudo[0]);
+                });
+                TournamentContainer.append(Players);
+                TournamentContainer.append(PlayersList);
+                if (TourInfo[0].state == 1) {
+                    var TitleStatut = $('<h1 style="font-size: 20px; margin-top:40px; margin-bottom:40px">Tournament statut : <b>In progress</b></h1>');
+                    TournamentContainer.append(TitleStatut);
+                    var games = data.games;
+                    if (games) {
+                        var phase = null;
+                        games.forEach(function (game) {
+                            if (phase !== game.phase) {
+                                phase = game.phase;
+                                var text = null;
+                                switch(game.phase) {
+                                    case 0:
+                                        text = "Final"
+                                        break;
+                                    case 1:
+                                        text = "Semifinals"
+                                        break;
+                                    case 3:
+                                        text = "Quarterfinals"
+                                        break;
+                                    default:
+                                        text = game.phase;
+                                        break;
+                                }
+                                var phase_txt =$('<h1 style="font-weight: bold; font-size: 28px">Phase ' + text + '</h1>');
+                                TournamentContainer.append(phase_txt);
+                            }
+                            var game_li =$('<li><span style="font-weight: bold; font-size: 24px">' + game.p1 + ' VS ' + game.p2 + '</span> </li>');
+                        
+                            if (game.state == 2) {
+                                var JoinGame = $('<button type="submit" style="margin-left: 25px;">Join the game</button>');
+                                JoinGame.click(function () {
+                                    var showDiv = document.getElementById('InfoTour');
+                                    showDiv.style.display = 'none';
+                                    import('/static/js/game.js?ver=${Math.random()}')
+                                    .then(module => {
+                                        const { reloadGame } = module;
+                                        reloadGame(game.id, game.p1, game.p2);
+                                    })
+                                    .catch(error => {
+                                        console.error('Une erreur s\'est produite lors du chargement de game.js : ', error);
+                                    });
+                                });
+                                game_li.append(JoinGame[0]);
+                            }
+                            else if (game.state == 3) {
+                                var game_wini =$('<span style="font-weight: bold; font-size: 24px; color:green;"> Winner : ' + game.winner + '</span>');
+                                game_li.append(game_wini);
+                            }
+
+                            TournamentContainer.append(game_li);
+                        });
+                    }
+                }
+                else {
+                    var TitleStatut = $('<h1 style="font-size: 20px; margin-top:40px; margin-bottom:40px">Tournament statut : <b>Not launched yet</b></h1>');
+                    TournamentContainer.append(TitleStatut);
+                }
+
+                if (data.owner) {
+                    if (TourInfo[0].state == 0) {
+                        var launchTour = $('<button type="submit" style="margin-right: 35px;">Launch the tournament</button>');
+                        launchTour.click(function () {
+                            TournamentUpdate(id, 'start');
+                            setTimeout(function() {
+                                TournamentInfo(id);
+                            }, 1000);
+                        });
+                        TournamentContainer.append(launchTour[0]);
+                    }
+                    var deleteTour = $('<button2 type="submit">Delete this tournament</button2>');
+                    deleteTour.click(function () {
+                        TournamentUpdate(id, 'delete');
+                        setTimeout(function() {
+                            getTournamentList();
+                            displayDiv('InfoTour', 'ChooseTour');
+                        }, 300);
+                    });
+                    TournamentContainer.append(deleteTour[0]);
+                }
+
+                var refreshTour = $('<div class="butt"><button type="submit" style="margin-top: 50px;">Refresh</button></div>');
+                refreshTour.click(function () {
+                    TournamentInfo(id)
+                });
+                TournamentContainer.append(refreshTour[0]);
+            }
+            token = data.csrf_token;
+        },
+        error: function (error) {
+            console.log('Can\'t load tournament info.');
+        }
+    });
+}
+
+function getTournamentList() {
+    $.ajax({
+        type: 'GET',
+        url: '/getTournamentList/',
+        headers: { 'X-CSRFToken': token },
+        success: function (data) {
+            if (data.success) {
+                var TournamentContainer = $('#TournamentsList');
+                var alreadyIn = false;
+                TournamentContainer.empty();
+                var friendsList = data.tourList;
+                friendsList.forEach(function (list) {
+                    if (list.me == list.id)
+                        alreadyIn = true;
+                });
+                friendsList.forEach(function (list) {
+                    var friendDiv =$('<li><span style="font-weight: bold; font-size: 24px">' + list.name + '</span> (' + list.players + ' players) - <i>created by ' + list.creator + '</i></li>');
+                    
+                    if (list.me != list.id && !alreadyIn) {
+                        var clickableRow = $('<button type="submit" style="margin-left: 25px;">Join</button>');
+                        clickableRow.click(function () {
+                            TournamentRegistration(list.id, true);
+                            setTimeout(function() {
+                                getTournamentList();
+                            }, 300);
+                        });
+                    friendDiv.append(clickableRow[0]);
+                    }
+                    else if (list.me == list.id) {
+                        var clickableRow = $('<button2 type="submit" style="margin-left: 25px;">Leave</button2>');
+                        clickableRow.click(function (event) {
+                            TournamentRegistration(list.id, false);
+                            setTimeout(function() {
+                                getTournamentList();
+                            }, 300);
+                        });
+                    friendDiv.append(clickableRow[0]);
+                    }
+
+                    var MoreInfo = $('<button type="submit" style="margin-left: 30px;">More info</button>');
+                    MoreInfo.click(function () {
+                        TournamentInfo(list.id);
+                    });
+                    friendDiv.append(MoreInfo[0]);
+                    TournamentContainer.append(friendDiv);
+                });
+            }
+            token = data.csrf_token;
+        },
+        error: function (error) {
+            console.log('Can\'t load tournament list.');
+        }
+    });
+}
+
 document.getElementById('JoinQueue').addEventListener('click', LaunchQueue);
 document.getElementById('LeaveQueue').addEventListener('click', LeaveQueue);
-
+document.getElementById('refreshTourList').addEventListener('click', getTournamentList);
+document.getElementById('Tournament').addEventListener('click', getTournamentList);
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,22 +1,5 @@
 var upHist = false;
-let chatCounter = 0;
 var token = getCSRFToken();
-
-const wsEndpoint = 'ws://' + window.location.host + '/ws/chat/';
-const websocket = new WebSocket(wsEndpoint);
-
-websocket.onopen = () => {
-    console.log('WebSocket connected');
-};
-
-websocket.onmessage = function(event) {
-    var data = JSON.parse(event.data);
-    console.log('Event Socket : ' + data.type);
-    if (data.type === 'chat_message') {
-        console.log('GetMessage');
-        getMessages();
-    }
-};
 
 function getCSRFToken() {
     var csrfTokenInput = document.querySelector('input[name="csrfmiddlewaretoken"]');
@@ -137,167 +120,20 @@ function clearInput(button) {
     }
 }
 
-function openChat(pseudo) {
-
-    if (chatCounter >= 5) {
-        alert("You have already 5 chat box !");
-        return;
-    }
-
-    var chatDiv = document.createElement('div');
-    chatDiv.id = 'chat-box-' + chatCounter;
-    chatDiv.id = pseudo;
-    chatDiv.className = 'chat-box';
-    chatDiv.style.left = 20 * chatCounter + 'vh';
-    var toggleDiv = document.createElement('div');
-    toggleDiv.className = 'chat-box-toggle';
-
-    var closeIcon = document.createElement('span');
-    closeIcon.className = 'close-icon';
-    closeIcon.innerHTML = '&times;';
-    toggleDiv.appendChild(closeIcon);
-
-    var titleP = document.createElement('p');
-    titleP.textContent = pseudo;
-    toggleDiv.appendChild(titleP);
-    chatDiv.appendChild(toggleDiv);
-
-    var contentDiv = document.createElement('div');
-    contentDiv.id = 'chat-box-content';
-    contentDiv.className = 'chat-box-content';
-    var chatP = document.createElement('p');
-    chatP.textContent = pseudo;
-    contentDiv.appendChild(chatP);
-
-    var formHTML = `
-        <form id="sendChatForm" enctype="multipart/form-data" action="/sendChat/" method="post">
-        <input type="hidden" name="type" value="sendChat">
-        <input type="hidden" name="id_to" value="${pseudo}">
-        <input type="hidden" name="csrfmiddlewaretoken" value="">
-        <input type="text" id="content" name="content" required>
-        <div class="hide"><button type="submit" onclick="sendForm('sendChatForm', event); clearInput(this)">Login</button></div>
-        <div id="error-form" class="error-form"></div>
-        </form>
-    `;
-    contentDiv.innerHTML = formHTML;
-    chatDiv.appendChild(contentDiv);
-
-    toggleDiv.addEventListener('click', function () {
-        chatDiv.classList.toggle('chat-box-open');
-        getMessages(); //Delete this later to setInterval
-        contentDiv.scrollTop = contentDiv.scrollHeight;
-    });
-    closeIcon.addEventListener('click', function (event) {
-        event.stopPropagation();
-        var parent = chatDiv.parentElement;
-        parent.removeChild(chatDiv);
-        chatCounter--;
-        var chatBoxes = document.getElementsByClassName('chat-box');
-        for (var i = 0; i < chatBoxes.length; i++) {
-            chatBoxes[i].style.left = 20 * i + 'vh';
-        }
-    });
-    document.body.appendChild(chatDiv);
-    chatCounter++;
-}
-
-function loadFriends() {
-    $.ajax({
-        type: 'GET',
-        url: '/getFriends/',
-        headers: { 'X-CSRFToken': token },
-        success: function (data) {
-            if (data.success) {
-                var friendsList = data.friends;
-                var friendsContainer = $('#friends');
-                friendsContainer.empty();
-                friendsList.forEach(function (friend) {
-                    var friendDiv =$('<div class="friends-list"></div>');
-                    var clickableRow = $('<div class="clickable-row name" data-pseudo="' + friend.pseudo + '">' + friend.pseudo + '</div>');
-                    clickableRow.click(function () {
-                        var chatDiv = document.getElementById(friend.pseudo);
-                        if (!chatDiv) {
-                            openChat(friend.pseudo);
-                            getMessages();
-                        }
-                    });
-                    var BlockRow = $('<div class="clickable-row" title="Block this friend" data-pseudo="' + friend.pseudo + '"><span class="close-icon">o</span></div>');
-                    BlockRow.click(function (event) {
-                        blockFriend(friend.pseudo, false);
-                    });
-                    var deleteRow = $('<div class="clickable-row" title="Delete this friend" data-pseudo="' + friend.pseudo + '"><span class="close-icon">×</span></div>');
-                    deleteRow.click(function (event) {
-                        deleteFriend(friend.pseudo);
-                    });
-                    friendDiv.append(clickableRow[0]);
-                    friendDiv.append(BlockRow[0]);
-                    friendDiv.append(deleteRow[0]);
-                    friendsContainer.append(friendDiv);
-                });
-            }
-            token = data.csrf_token;
-        },
-        error: function (error) {
-            console.log('Erreur lors de la récupération des amis.');
-        }
-    });
-}
-
-function loadBlockedFriends() {
-    $.ajax({
-        type: 'GET',
-        url: '/getBlockedFriends/',
-        headers: { 'X-CSRFToken': token },
-        success: function (data) {
-            if (data.success) {
-                var friendsList = data.friends;
-                var friendsContainer = $('#blocked-friend');
-                friendsContainer.empty();
-                friendsList.forEach(function (friend) {
-                    var friendDiv =$('<div class="friends-list"></div>');
-                    var clickableRow = $('<div class="clickable-row name" data-pseudo="' + friend.pseudo + '">' + friend.pseudo + '</div>');
-                    clickableRow.click(function () {
-                        // var chatDiv = document.getElementById(friend.pseudo);
-                        // if (!chatDiv) {
-                        //     openChat(friend.pseudo);
-                        //     getMessages();
-                        // }
-                    });
-                    var BlockRow = $('<div class="clickable-row" title="Block this friend" data-pseudo="' + friend.pseudo + '"><span class="close-icon">o</span></div>');
-                    BlockRow.click(function (event) {
-                        blockFriend(friend.pseudo, true);
-                    });
-                    friendDiv.append(clickableRow[0]);
-                    friendDiv.append(BlockRow[0]);
-                    friendsContainer.append(friendDiv);
-                });
-            }
-            token = data.csrf_token;
-        },
-        error: function (error) {
-            console.log('Erreur lors de la récupération des amis.');
-        }
-    });
-}
-
-function deleteFriend(pseudo) {
+function TournamentUpdate(id, statut) {
     var formData = new FormData();
-    formData.append('type', 'addFriend');
-    formData.append('delete', 'true');
-    formData.append('addfriendName', pseudo);
-
+    formData.append('type', 'tourUpdate');
+    formData.append('id', id);
+    formData.append('statut', statut);
     $.ajax({
         type: 'POST',
-        url: '/addFriend/',
+        url: '/tourUpdate/',
         headers: { 'X-CSRFToken': token },
         processData: false,
         contentType: false,
         data: formData,
         success: function (data) {
-            if (data.success) {
-                console.log('friend deleted');
-            }
-            loadFriends();
+            $('#error-tour').text(data.errors);
             token = data.csrf_token;
         },
         error: function (error) {
@@ -306,27 +142,19 @@ function deleteFriend(pseudo) {
     });
 }
 
-function blockFriend(pseudo, unblock) {
+function TournamentRegistration(id, join) {
     var formData = new FormData();
-    formData.append('type', 'blockFriend');
-    formData.append('block', (unblock ? 'false' : 'true'));
-    formData.append('blockFriendName', pseudo);
-
+    formData.append('type', 'tourRegist');
+    formData.append('id', id);
+    formData.append('join', (join ? 'true' : 'false'));
     $.ajax({
         type: 'POST',
-        url: '/blockFriend/',
+        url: '/tourRegist/',
         headers: { 'X-CSRFToken': token },
         processData: false,
         contentType: false,
         data: formData,
         success: function (data) {
-            if (data.success)
-                $('#error-block').text(data.errors);
-            else
-                $('#error-block').text(data.errors);
-            console.log(data.errors)
-            loadFriends();
-            loadBlockedFriends();
             token = data.csrf_token;
         },
         error: function (error) {
@@ -366,11 +194,23 @@ function loadStats() {
 
 function printStats(user) {
     var statCont = document.getElementById('stats-users-container');
-    var userList = document.createElement('div');
-    userList.className = 'users-list';
-
     var list = document.createElement('tr');
-    userList.appendChild(list);
+    list.className = 'users-list';
+    list.onclick = function () {
+        document.getElementById('profil-card').style.display = 'flex';
+        document.getElementById('profile').style.display = 'inline-flex';
+        document.getElementById('profil-card').classList.toggle('profil-open');   
+        $('#user-name').text(user.username);
+        $('#user-pseudo').text(user.pseudo);
+        $('#user-email').text(user.email);
+        $('#user-img').attr('src', user.img);
+        document.getElementById('profil-card').addEventListener( "click", () => {
+            document.getElementById('profil-card').classList.toggle('profil-open');   
+            document.getElementById('profil-card').style.display = 'none';
+            document.getElementById('profile').style.display = 'none';
+        });
+        
+    }
 
     var attr = document.createElement('td');
     attr.className = 'pseudo';
@@ -378,7 +218,7 @@ function printStats(user) {
     list.appendChild(attr);
     var attr2 = document.createElement('td');
     attr2.className = 'img';
-    var img = document.createElement('td');
+    var img = document.createElement('img');
     img.src = user.img;
     attr2.appendChild(img);
     list.appendChild(attr2);
@@ -391,11 +231,7 @@ function printStats(user) {
     attr4.textContent = user.mmr;
     list.appendChild(attr4);
 
-    userList.addEventListener('click', function () {
-        var url = window.location.href + '#' + user.pseudo;
-    })
-    // statCont.innerHTML;
-    statCont.appendChild(userList);
+    statCont.appendChild(list);
 }
 
 window.addEventListener('hashchange', function () {
@@ -425,44 +261,6 @@ document.getElementById('profil-img').addEventListener('change', function (event
     sendForm('profilImg', event)
 });
 
-function getMessages() {
-    $.ajax({
-        type: 'GET',
-        url: '/getChat/',
-        headers: { 'X-CSRFToken': token },
-        success: function (data) {
-            var messages = data.messages;
- 
-            var chatBoxContents = document.querySelectorAll('.chat-box-content');
-            chatBoxContents.forEach(function (chatBoxContent) {
-                var paragraphs = chatBoxContent.querySelectorAll('p');
-                paragraphs.forEach(function (paragraph) {
-                    paragraph.remove();
-                });
-                messages.forEach(function (message) {
-                    var id = message.pseudo_from === message.me ? message.pseudo_to : message.pseudo_from;
-                    var chatDiv = document.getElementById(id);
-                    if (!chatDiv)
-                        return;
-                    var chatBoxContent = chatDiv.querySelector('.chat-box-content');
-                    if (!chatBoxContent)
-                        return;
-                    var messageElement = document.createElement('p');
-                    
-                    var from = message.pseudo_from === message.me ? '<b>Moi : </b>' : '<b>' + message.pseudo_from + ' : </b>';
-                    messageElement.innerHTML = from + message.content;
-
-                    chatBoxContent.appendChild(messageElement);
-                    chatBoxContent.scrollTop = chatBoxContent.scrollHeight;
-                });
-            });
-            token = data.csrf_token;
-        },
-        error: function (error) {
-            console.log('Erreur lors de la récupération des amis.');
-        }
-    });
-}
 
 function displayDiv(hide, show) {
     var hideDiv = document.getElementById(hide);
@@ -478,6 +276,3 @@ window.onload = function() {
     loadFriends();
     loadBlockedFriends();
 };
-
-
-// setInterval(getMessages, 1000);
