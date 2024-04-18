99f41308475f89f29b0090e8ca2552932535950c
Lucas <lu.ca.deville@outlook.fr>
2024-04-18 15:41:52 +0200

    Blockchain almost done

--- app/consumer.py
+++ app/consumer.py
@@ -392,13 +392,10 @@ class MyGameConsumer(AsyncWebsocketConsumer):
                             'gasPrice': web3.to_wei('50', 'gwei'),
                             'nonce': web3.eth.get_transaction_count(account_address),
                         })
-                        # Signer la transaction avec votre clé privée
                         signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
 
-                        # Envoyer la transaction signée
                         tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
-                        # Attendre que la transaction soit confirmée
                         tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
                         print("Transaction envoyée. Hash de transaction:", tx_hash.hex())
 
