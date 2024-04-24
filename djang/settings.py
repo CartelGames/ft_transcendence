669f8654252316a1957736cc3e4d7237b7d665df
Lucas Deville <ldeville@c2r7p5.42perpignan.fr>
2024-04-24 14:09:16 +0200

    Fix img

--- djang/settings.py
+++ djang/settings.py
@@ -16,10 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(v!v*p&ci0025(nl+heca_tstmaijit_*((c_vk_17=jna#q#j'
 
